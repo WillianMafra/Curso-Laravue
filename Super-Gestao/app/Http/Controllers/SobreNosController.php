addbaaddbf30f8b99903762442478c83237df7f0
Willian Matheus Nunes Mafra <williannunes965@gmail.com>
2022-11-23 20:26:49 -0300

    rotas e controllers

---
+++ Super-Gestao/app/Http/Controllers/SobreNosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SobreNosController extends Controller
+{
+    public function sobrenos() {
+        echo 'Rota Sobre Nós';
+    }
+}
