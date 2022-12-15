277d6712e625d7511e9717846b4a4f27f29d00d6
Willian Matheus Nunes Mafra <williannunes965@gmail.com>
2022-12-15 18:40:48 -0300

    Adicionando: Méetodo fallback e testando passagem de parametros via get

--- Super-Gestao/resources/views/site/principal.blade.php
+++ Super-Gestao/resources/views/site/principal.blade.php
@@ -1,6 +1,6 @@
 <h3>Principal (view)</h3>
 <ul>
-    <li><a href="/contato">Contato</a></li>
-    <li><a href="/sobre-nos">Sobre Nós</a></li>
-    <li><a href="/">Home</a></li>
+    <li><a href="{{ route('site.contato') }}">Contato</a></li>
+    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+    <li><a href="{{ route('site.index') }}">Home</a></li>
 </ul>
--- Super-Gestao/routes/web.php
+++ Super-Gestao/routes/web.php
@@ -13,19 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobrenos']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
 // Usando o método prefix para definir o prefixo no get e o metodo group para definir as rotas que serão agrupadas
 Route::prefix('/app')->group( function() {
-    Route::get('/login', [\App\Http\Controllers\ContatoController::class, 'login']);
-    Route::get('/clientes', [\App\Http\Controllers\ContatoController::class, 'clientes']);
-    Route::get('/fornecedores', [\App\Http\Controllers\ContatoController::class, 'fornecedores']);
-    Route::get('/produtos', [\App\Http\Controllers\ContatoController::class, 'produtos']);
+    Route::get('/login', [\App\Http\Controllers\ContatoController::class, 'login'])->name('app.login');
+    Route::get('/clientes', [\App\Http\Controllers\ContatoController::class, 'clientes'])->name('app.clientes');
+    Route::get('/fornecedores', [\App\Http\Controllers\ContatoController::class, 'fornecedores'])->name('app.fornecedores');
+    Route::get('/produtos', [\App\Http\Controllers\ContatoController::class, 'produtos'])->name('app.produtos');
 });
+Route::get('/teste/{p1}/{p2}', [ \App\Http\Controllers\TesteController::class, 'teste']);
 
 Route::get('/contato/{nome}/{idade}', function( string $nome = 'Desconhecido', int $idade = 0) {
     echo 'Nome: '.$nome . ' Idade: ' . $idade; 
 })->where('idade', '[0-9]+')->where('nome', '[A-Za-z]+');
 // Metódo where utilizado para validacão de dados recebidos pelo Get
+
+Route::fallback(function( ){
+    echo 'Endereço não encontrado, <a href="/">clique aqui</a> para voltar ao início';
+});
+// Método fallback utilizado para redirecionar o usuário para uma página específica, caso a rota utilizada não exista
